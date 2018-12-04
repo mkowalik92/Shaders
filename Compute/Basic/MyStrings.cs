55f60df1c5e5948f5a20e7b9297bc893946c3e3d
Marek Kowalik <mkowalik92@gmail.com>
2018-12-04 14:52:22 -0500

    Create MyStrings.cs

---
+++ Compute/Basic/MyStrings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MyStrings
+{
+    public const string ComputePractice = "ComputePractice";
+    public const string FillWithRed = "FillWithRed";
+    public const string result = "result";
+}
